9749ab41ec91fc5694a4049c9b03706e1ea37252
cinna <j4zzab@gmail.com>
2025-01-06 02:37:37 +0000

    Update todo.py
    Added custom name usage, and fixed some weird behaviour when adding or deleting tasks

--- todo/todo.py
+++ todo/todo.py
@@ -3,29 +3,55 @@
 import os
 import time
 
-path = "C:\\cinnastartup\\todo\\"
+# Files
 
-os.system("C:\\cinnastartup\\todo\\time.bat")
-timeFile = open("C:\\cinnastartup\\todo\\time.txt", "r")
-listFile = open("C:\\cinnastartup\\todo\\list.txt", "r")
-# For some reason, if you don't put the entire path to the file, python goes a little schizo.
+tempPath = __file__.split("\\")
+dir = ""
+for i in range(0, len(tempPath)-1):
+    dir += tempPath[i] + "\\"
 
-time.sleep(0.001)
+print(dir)
 
-currentHour = timeFile.read()
+os.system(f"{dir}time.bat") # Needs to be run in advance of timeFile opening
+setupFile = open(f"{dir}setup.txt", "r")
+timeFile = open(f"{dir[:-5]}time.txt", "r")
+listFile = open(f"{dir}list.txt", "r")
+
+setupContent = setupFile.readlines()
+timeContent = timeFile.readlines()
+listContent = listFile.readlines()
+
+# Var (idk if this is needed)
+
+name = ""
+
+# Main
+
+if setupContent[0] == "0":
+
+    # Start setup
+    print("This is your first run of todo!")
+    setupFile = open(f"{dir}setup.txt", "w")
+
+    name = input("Enter name to use: ")
+    setupFile.write(f"{name}\n")
+
+# Normal run
+
+name = setupContent[0].replace("\n", "")
+
+currentHour = str(timeContent[0])[:-2]
 if int(currentHour) < 12:
-    print("Good morning, James!\n")
+    print(f"Good morning, {name}!\n")
 else:
-    print("Good evening, James!\n")
-
-tasks = listFile.readlines()
+    print(f"Good evening, {name}!\n")
     
 loop = True
 while loop:
-    print("Here are your tasks as of now:")
-    for l in range(0, len(tasks)):
-        string = f"{l+1} : {tasks[l].replace("\n", "")}"
-        if l+1 < 10: string = "0" + string
+    print("Your to-do list:")
+    for task in range(0, len(listContent)):
+        string = f"{task+1} : {listContent[task].replace("\n", "")}"
+        if task+1 < 10: string = "0" + string
         print(string)
 
     action = input("\nAdd task, Remove task, or Exit? [a/r/e]: ")
@@ -36,15 +62,15 @@ while loop:
         case "a":
             index = int(input("Enter list index: "))
             newTask = input("Enter new task: ")
-            tasks.pop(index-1)
-            tasks.insert(index-1, newTask)
-            tasks.append("")
+            listContent.pop(index-1)
+            listContent.insert(index-1, newTask+"\n")
 
         case "r":
             index = int(input("Enter list index: "))
-            confirm = input(f"\n <!> Are you sure you want to remove task {index+1}? \n Type \"DELETE\" to confirm: ")
+            confirm = input(f"\n <!> Are you sure you want to remove task {index}? \n Type \"DELETE\" to confirm: ")
             print("Deleting index...")
-            tasks.pop(index-1)
+            listContent.pop(index-1)
+            listContent.append("\n")
 
         case "e":
             print("Exiting...")
@@ -53,12 +79,10 @@ while loop:
         case _:
             print("That command isn't recognised!")
 
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("\n\n\n")
 
-listFile = open("C:\\cinnastartup\\todo\\list.txt", "w")
-for j in range(0, 16):
-    if tasks[j]:
-        listFile.writelines(f"{tasks[j]}")
-    else:
-        listFile.writelines("\n")
+listFile = open(f"{dir}list.txt", "w")
+print(listContent)
+for j in range(0, len(listContent)):
+    listFile.writelines(f"{listContent[j]}")
 listFile.close()
